c6f3f081b62d9bc1de829a6926debe21cdd0c533
Kyle <kyleryan480@gmail.com>
2018-07-31 17:44:17 -0700

    added gravatar + working registration route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,7 +4,6 @@ const router = express.Router();
 // @route   Get api/profile/test
 // @desc    Tests profile route
 // @access  Public
-
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
 module.exports = router;
